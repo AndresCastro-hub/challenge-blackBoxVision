a06586f2818633ae52dad530b82b7187435542f1
Andres Castro <andrescastro2011@hotmail.com>
2022-06-25 16:25:56 -0300

    consumiendo los datos de la api

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,42 @@
-import Button from "./components/button/Button"
+import { useEffect, useState } from "react";
 import './app.css'
+import Button from "./components/button/Button"
 import QuestionCard from "./components/questionCard/QuestionCard"
+import getQuestions from "./services/api";
+import {Question } from './services/types';
 
 function App() {
+
+  const [questions, SetQuestions] = useState<Question[]>();
+  const [preguntaActual , setPreguntaActual] = useState<number>(0);
+  const [loading, setLoading]  = useState<boolean>(false)
+  const [puntuacion, setPuntuacion ] = useState<number>(0)
+  const [estado, setEstado ] = useState<"Cargando"|"Cargado"|"Finalizado">("Cargando");
+
+  useEffect(() => {
+    getQuestions()
+    .then(
+      (res) => { SetQuestions(res)
+      setEstado("Cargado")
+    })
+  }, []);
+
+  if(estado === "Cargando"){
+    return <div className="loading">Cargando...</div>
+  }
+
+  const question = questions[preguntaActual]
 
   return (
     <main className="contenedor">
       <section className="estructura">
-        <QuestionCard header="CATEGORY" footer="2 - 10">
+        <QuestionCard 
+          header={`${question.category} - ${question.difficulty} `} 
+          footer={`${[preguntaActual] + 1} - ${questions.length}` }>
           ¿Como se llama el protagonista de the legend of zeld?
         </QuestionCard>
         <nav className="respuestas">
-          <Button>Zelda</Button>
-          <Button>Link</Button>
-          <Button>Peter Pan</Button>
-          <Button>El duende verde</Button>
+          {[...question.incorrect_answers, question.correct_answer].map(respuesta => <Button key={respuesta}>{respuesta}</Button>)}
         </nav>
       </section>
     </main>
